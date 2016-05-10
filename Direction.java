771256ece94f76c7fc9898c0355922acbd04fbec
Dohryk <mars207@mail.ru>
2016-05-10 16:32:17 +0300

    Create Direction.java

---
+++ Direction.java
@@ -0,0 +1,14 @@
+package Tanker;
+
+/**
+ * Created by vdohryk on 10.05.2016.
+ */
+public enum Direction {
+
+    NONE(0), UP(1), DOWN(2), LEFT(3), RIGHT(4);
+    private int id;
+
+    private Direction(int id){
+        this.id = id;
+    }
+}
