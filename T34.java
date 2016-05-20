3d66b788f2787ce50a9238925b9cdc4ffa23bb02
Dohryk <mars207@mail.ru>
2016-05-20 21:22:43 +0300

    tiger armour

---
+++ T34.java
@@ -0,0 +1,32 @@
+package Tanker;
+
+public class T34 extends Tank{
+    private int maxSpeed = 50;
+    private int crew = 4;
+    private TankColor color = TankColor.GREEN;
+
+    public T34(ActionField actionField, BattleField battleField, int x, int y, Direction direction){
+        super(actionField, battleField, x, y, direction);
+    }
+    public T34(int maxSpeed, int crew, TankColor color){
+        this.maxSpeed = maxSpeed;
+        this.crew = crew;
+        this.color = color;
+    }
+
+    public void printInfo(T34 tank) {
+
+        System.out.println(tank.toString() + "[color:" + tank.getColor() + " , crew:" + tank.getCrew() +
+                " , maxSpeed:" + tank.getMaxSpeed()+"]");
+    }
+
+    @Override
+    public String toString() {
+        return "T34 ";
+    }
+
+    @Override
+    public void move() throws Exception {
+        super.move();
+    }
+}
