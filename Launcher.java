e80a523a0a6a1856dc839ac597fee44141aa712e
Dohryk <mars207@mail.ru>
2016-05-20 20:07:02 +0300

    Create Launcher.java

---
+++ Launcher.java
@@ -0,0 +1,10 @@
+package Tanker;
+
+public class Launcher {
+
+    public static void main(String[] arg) throws Exception {
+        ActionField actionField = new ActionField();
+        actionField.runTheGame();
+    }
+
+}
